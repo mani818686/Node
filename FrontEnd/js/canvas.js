507dacaeabf0b4a9014e0fc03752b82973e5d0c4
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-07-28 23:28:25 +0530

    Updated

--- FrontEnd/js/canvas.js
+++ FrontEnd/js/canvas.js
@@ -1,13 +1,25 @@
-var c=document.getElementById('Canvas');
-var ctx=c.getContext("2d");
-const circle={
-    x:200,
-    y:200,
-    dx:5,
-    dy:5;
-}
+var canvas=document.getElementById("Canvas");
+var ctx=canvas.getContext("2d");
+/*ctx.beginPath();
+ctx.arc(canvas.width/2,canvas.height/2,220, 0, 2 * Math.PI);
+ctx.stroke();*/
+
+var x=200,y=300,r=9;
 function drawcircle()
 {
-ctx.drawcircle(x,y,50,0,2*Math.PI);
-ctx.stroke();
+ctx.beginPath();
+ctx.arc(x,y,r,0,Math.PI*2);
+ctx.fillStyle="purple";
+ctx.fill();
 }
+function update()
+{   ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    x+=5;
+    y+=2;
+    drawcircle();
+    
+    console.log("HIT");
+    requestAnimationFrame(update);
+}
+update();
