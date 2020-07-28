e690e6027ba106b1cb45ca330f786498e2524e15
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-07-28 18:50:53 +0530

    Updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express=require('express');
 var app=express();
+app.use(express.static(__dirname+'/FrontEnd/'))
+var port=process.env.PORT||3000;
 app.get('/',function(req,res)
 {
     res.sendFile('html/index.html');
 })
-app.use(express.static(__dirname+'/FrontEnd/'))
-var port=process.env.PORT||3000;
 app.listen(port,function()
 {
     console.log("site running on localhost "+port);
