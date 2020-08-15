cd140dff1e6925d422c89867eef5bd636be531b7
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-08-15 18:28:57 +0530

    Updated string again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var app=express();
 app.use(express.json());
-app.use(express.urlencoded())
+//app.use(express.urlencoded())
 var userLib=require('./userlib')
 var db=require('./Backend/db/dbconnect');
 db.connect();
