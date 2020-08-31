10ae3552edfba74aa2b8de38d1e7a5006e7d39fe
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-08-31 20:00:30 +0530

    added local storage

--- FrontEnd/js/profile.js
+++ FrontEnd/js/profile.js
@@ -1,22 +1,32 @@
+
 $(document).ready(function(){
+    var username=window.localStorage.getItem("username");
+    username=JSON.parse(username);
+    if(!username){
     $.ajax({
         url: '/user',
         method: 'GET'
     })
     .then(function(result){
-        //console.log(result[0].username+" "+result.length);
+        window.localStorage.setItem("username",JSON.stringify(result[0].username));
         if(result.length==0)
             $(".username").text("");
         else{
             $(".username").text(result[0].username);
         }
-          
-    })
+        
+    })}
+  else
+{   console.log("local storage")
+    $(".username").text(username);
+}
 })
+
 
 
 function display()
 {
+    window.localStorage.removeItem("username");
     $(".username").text("");
         window.location = '/login';
         $.ajax({
@@ -24,6 +34,7 @@ function display()
             method: 'POST'
         })
         .then(function(result){
+            
             window.location.redirectUrl = result.redirectUrl;
         })
 }
